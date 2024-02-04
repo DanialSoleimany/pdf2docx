58fd3d9a9fb978743b2ed4f5a14234dbcfffd3b1
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2024-02-04 09:34:08 +0330

    Update Converter.py

--- Converter.py
+++ Converter.py
@@ -1,5 +1,5 @@
 from pdf2docx import Converter
 
-cv = Converter("pdf_name.pdf")
-cv.convert("output_docx_name.docx", start=0, end=None)
+cv = Converter("input_file.pdf")
+cv.convert("output_file.docx", start=0, end=None)
 cv.close()
